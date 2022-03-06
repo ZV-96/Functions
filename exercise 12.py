a234f5ed0b66f53a75c923eb8197143b3c5704f8
ZV-96 <vanrooyenz@middleton.school.nz>
2022-03-07 12:01:39 +1300

    Create exercise 12.py

---
+++ exercise 12.py
@@ -0,0 +1,48 @@
+
+# This function runs the main program and coordinates calls to other functions 
+
+
+def main_routine():
+    adult_tickets = 0
+    student_tickets = 0
+    child_tickets = 0
+    gift_tickets = 0
+    total_sales = 0
+    tickets_sold = 0
+
+
+ticket wanted = input("Do you want to sell a ticket? (YIN):
+while ticket wanted != "N":
+ticket z
+upper()
+num_tickets int many of these tickets do you want:
+confirm purchase of {num_tickets} type {ticket} "
+f"ticket(s)? (YIN):
+upper()
+if confirm
+tt Giving user the option to cancel the sale
+print
+print
+price num_tickets float(eet_price(ticket))
+total_sales +z price
+tickets sold num tickets
+if ticket
+adult tickets +z num tickets
+elif ticket
+student tickets nun tickets
+elif ticket
+child tickets num tickets
+gift_tickets num_tickets
+ticket_wanted you want to sell "
+"another ticket? (YIN):
+" upper()
+total tickets sold today was
+f" This was made up Of: in"
+for adults; and
+for students; and
+for children; and
+gift vouchers in")
+for the day came to .2f)")
+# Get the Category Of ticket the purchaser wants
+def ) :
+page l, last modified 03/01/22
