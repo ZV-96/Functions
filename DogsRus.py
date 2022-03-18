dcee6e9783dd3e7d34f8fd73de09422664490670
ZV-96 <vanrooyenz@middleton.school.nz>
2022-03-19 11:02:45 +1300

    Update

---
+++ DogsRus.py
@@ -0,0 +1,8 @@
+def main_function():
+    print("--------------- Welcome to DogsRus Dog sitting ---------------")
+    print("What do you want to do today: ")
+    print("1 Drop off a dog")
+    print("2 pick up a dog")
+    print("3 calculate cost")
+    print("4 print dog list")
+    print("5 quit")
